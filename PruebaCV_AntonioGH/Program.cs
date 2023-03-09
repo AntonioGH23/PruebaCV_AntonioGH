cb4c59cf73144e855e7a7b1ecdad9d7ca9caa339
AntonioGH23 <a22antoniogui@iesluiscarrillodesotomayor.org>
2023-03-09 10:57:16 +0100

    Suma de dos numeros

--- PruebaCV_AntonioGH/Program.cs
+++ PruebaCV_AntonioGH/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
+            int numero;
+            int numero2;
+            int suma;
+            numero = 6;
+            numero2 = 76;
+
             Console.WriteLine("Antonio");
+            suma = numero + numero2;
+            Console.WriteLine(suma);
+
         }
     }
 }
